5452b5a9c5efefe48a2e08cb4cc0f0604edef877
vladsilashko <v.silashko@gmail.com>
2022-11-19 21:59:40 +0200

    library datetime

---
+++ name.py
@@ -0,0 +1,4 @@
+import datetime
+zminna_date_time = datetime.datetime(2022, 11, 19, hour=21, minute=54, second=34, microsecond=434)
+print(f'object datetime-{zminna_date_time}')
+print(f'type-{type(zminna_date_time)}')
